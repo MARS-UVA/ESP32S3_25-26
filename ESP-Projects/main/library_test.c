3736656eb4176e246c14f2feec317570c438131a
lukeholiver <ttr3te@virginia.edu>
2025-10-19 15:56:51 -0400

    Updated little endian formating for current limit

--- ESP-Projects/main/library_test.c
+++ ESP-Projects/main/library_test.c
@@ -1,15 +1,10 @@
 #include "LO_CAN.h"
 
 
-
 void app_main(){
 
     canSetUp();
-    
-    while (1) {
-        talonPercentOut(112); // %50 of motor output
-        vTaskDelay(5); // watchdog -> way to know if microcontroller still works fine by feeding it code
-            // if we don't feed the watchdog for a certain amount of time, the code crashes
-    }
+    supplyCurrentLimit(.5);
+    talonPercentOut(20);
 
 }
