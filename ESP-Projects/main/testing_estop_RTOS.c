814e9b64264e0ef20ff422e071400161700ae787
ColeLuba <meq2fg@virginia.edu>
2025-11-05 18:52:01 -0500

    Update testing_estop_RTOS.c
    Added uart reading

--- ESP-Projects/main/testing_estop_RTOS.c
+++ ESP-Projects/main/testing_estop_RTOS.c
@@ -6,6 +6,23 @@
 TaskHandle_t myTaskHandle1 = NULL;
 TaskHandle_t myTaskHandle2 = NULL;
 
+void init(void) 
+{
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM, &uart_config);
+    uart_set_pin(UART_NUM, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
 void canRunRTOS(void *arg){
     while (1)  {
     talonPercentOut(100);   
@@ -29,9 +46,13 @@ void readButton(){
 }
 
 void app_main(){
-
+    init();
     gpio_set_direction(BUTTONPIN, GPIO_MODE_INPUT);
     canSetup();
+
+    if(uart_read_bytes(UART_NUM, NULL, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS) > 0){
+        vTaskSuspend(myTaskHandle1);
+    }
 
     xTaskCreatePinnedToCore(canRunRTOS, "canRunRTOS", 4096, NULL, 7, &myTaskHandle1, 1);
     xTaskCreatePinnedToCore(readButton, "readButton", 4096, NULL, 1, &myTaskHandle2, 1);
