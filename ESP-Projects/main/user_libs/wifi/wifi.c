8c481595b7db3743415257152ae78df59199a4ee
Eric Paschke <edpaschke@gmail.com>
2025-12-03 18:11:43 -0500

    changes to wifi library init

--- ESP-Projects/main/user_libs/wifi/wifi.c
+++ ESP-Projects/main/user_libs/wifi/wifi.c
@@ -1,25 +1,27 @@
 #include "esp_wifi.h"
 
-/* ------------- Wfi Configs -------------*/
 
-wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-wifi_mode_t mode = WIFI_MODE_STA;
-wifi_config_t wifi_config = {
-    .sta = {
-        .ssid = "YOUR_SSID",
-        .password = "YOUR_PASSWORD",
-    }
-};
+void setupWifi() {
+    /* ------------- Wfi Configs -------------*/
+    const wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    wifi_mode_t mode = WIFI_MODE_STA;
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "YOUR_SSID",
+            .password = "YOUR_PASSWORD",
+        }
+    };
 
-/* ------------- Initalize Wfi -------------*/
+    /* ------------- Initalize Wfi -------------*/
 
 
-ESP_ERROR_CHECK(esp_wifi_init(const wifi_init_config_t *config))
-ESP_ERROR_CHECK(esp_wifi_set_mode(wifi_mode_t mode))
-ESP_ERROR_CHECK(esp_netif_create_default_wifi_sta())
-ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config))
-ESP_ERROR_CHECK(esp_wifi_start())
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(mode));
+    esp_netif_create_default_wifi_sta();
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
-/* ------------- Connect to Wfi -------------*/
+    /* ------------- Connect to Wfi -------------*/
 
-ESP_ERROR_CHECK(esp_wifi_connect())
+    ESP_ERROR_CHECK(esp_wifi_connect());
+}
