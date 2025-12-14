196139550396877a5d3116106d31e8815b050bf2
fredm150 <mensahfred150@gmail.com>
2025-12-14 16:01:49 -0500

    added new functions

--- ESP-Projects/main/user_libs/ADC/adc.c
+++ ESP-Projects/main/user_libs/ADC/adc.c
@@ -5,41 +5,39 @@
 #include <string.h>
 #include "esp_log.h"
 #include <driver/adc.h>
+#include "esp_err.h"
 
 
 
-#define POT_CALIBRATION_COUNT 20            // change this to the ESP equivalent
 
-esp_err_t ret;
-    uint32_t ret_num = 0;
-    uint8_t result[256] = {0};
-    memset(result, 0xcc, 256);
     
 
 // read a decimal value between 0 and 1 indicating position of potentiometer
 float readPot(Pot* pot) {
-	adc_continuous_start(pot->handle);
-	// adc_continuous_read(pot->handle, 20);
     
-     ret = adc_continuous_read(handle, result, 256, &ret_num, 0);
-     uint32_t data;
+    esp_err_t ret;
+    uint8_t result[256];          // this line makes a new array called 'result' of size 256 bytes
+    uint32_t ret_num = 0;          // this variable will hold the number of bytes read from the ADC
+    uint32_t data_samples = 0;
+    memset(result, 0xcc, 256);    // set all elements at results buffer to value 0xcc
+
+     ret = adc_continuous_read(pot->handle, result, 256, &ret_num, 0);
 
             if (ret == ESP_OK){
                 for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES)
                 {
                     adc_digi_output_data_t *p = (adc_digi_output_data_t *)&result[i];
-                    data = p->type2.data
-                    printf("Value:\t%ld\n", data);
+                    data_samples = p->type2.data;
+                    printf("Value:\t%ld\n", data_samples);
                 }
                 vTaskDelay(pdMS_TO_TICKS(100));     // Add delay between reads
-    
-    // 
-	return map(pot->minPos, pot->maxPos, data + pot->actuatorOffset); }
+     
+	return map(pot->minPos, pot->maxPos, data_samples + pot->actuatorOffset); }
 }
 
 
-# 
-Pot PotInit(ADC_HandleTypeDef *handle) {
+// this function initializes a Potentiometer struct and its ADC handle 
+Pot PotInit(adc_continuous_handle_t *handle) {
 	Pot pot = {
 		.handle = handle,
 		.read = readPot,
@@ -47,7 +45,7 @@ Pot PotInit(ADC_HandleTypeDef *handle) {
 		.maxPos = 3153,
 	};
 
-	adc_continuous_handle_start(handle);
+	adc_continuous_start(pot->handle);
 
     // this is how you intialize  the ADC Continous Driver mode 
     adc_continuous_handle_t handle = NULL;
@@ -57,4 +55,15 @@ Pot PotInit(ADC_HandleTypeDef *handle) {
 };  
 
 	return pot;
+}
+
+
+// Deinitialize the Potentiometer's ADC handle --> Helpers to avoid memory leaks
+void PotDeInit(Pot* pot) {
+    if (pot != NULL && pot->handle != NULL) {
+        adc_continuous_stop(pot->handle);       // Stop ADC continuous reading
+        adc_continuous_delete_handle(pot->handle);      // Delete ADC handle to allow free memory
+        pot->handle = NULL;
+        ESP_LOGI(TAG, "Potentiometer ADC Deinitialized.");
+    }
 }
--- ESP-Projects/main/user_libs/ADC/adc.h
+++ ESP-Projects/main/user_libs/ADC/adc.h
@@ -1,20 +1,30 @@
 #include "main.h"
 #include "utils.h"
+#include "adc.h"
+#include`<esp_adc/adc_continuous.h>
 
 
+
+
+#define ADC_H
 
 
 typedef struct pot {
-	ADC_HandleTypeDef *handle;
+	adc_continuous_handle_t *handle;
 	float(*read)(struct pot*);
 	float(*readCm)(struct pot*);
 	uint32_t actuatorOffset;
-	int minPos;
-	int maxPos;
+	int minPos;				// minimum ADC reading
+	int maxPos;				// maximum ADC reading
 } Pot;
 
-Pot PotInit();
+Pot PotInit(void);
+
+float readPot(Pot* pot);
 
 void calibrateYourMom(Pot *leftPot, Pot *rightPot);
+
+void PotDeInit(Pot* pot);
+
 
 #endif
