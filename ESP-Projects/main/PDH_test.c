8d851a37f9dc9e04b6b47f4f04daa141dbf17261
Carlos Giron <carlosegiron92@gmail.com>
2026-02-02 02:10:52 -0500

    Conducted pdp te4st in PDH_test.c

--- ESP-Projects/main/PDH_test.c
+++ ESP-Projects/main/PDH_test.c
@@ -4,17 +4,32 @@
 
 #include <stdio.h>
 
+TalonFX motor;
+PDP pdp;
+
+void motor_task()
+{
+    motor = talonFXInit(1, 7);
+    while (1)
+    {
+        setFX(&motor, 0.5);
+    }
+}
+
+void current_sensing()
+{
+    float cur;
+    PDPInit(&pdp, 10);
+    canSetupPDP(&pdp);
+    while (1)
+    {
+        requestCurrentReadingsPDP(&pdp, 10);
+        cur = getChannelCurrentPDP(&pdp, 7);
+        printf("Current at channel %d is:\t %.3f\n", pdp.identifier, cur);
+    }
+}
 void app_main()
 {
-    TalonFX motor = talonFXInit(36, 0);
-    PDP pdp;
-    uint32_t pdp_id = 0;
-    canSetupPDP(&pdp);
-    
-
-    for (int i = 0; i < 60; i++)
-    {
-        printf("%d\n", i);
-        setTargetFX(&motor, 100);
-    }
+    xTaskCreate(current_sensing, "curr", 4096, NULL, 9, NULL);
+    xTaskCreate(motor_task, "mtr", 4096, NULL, 8, NULL);
 }
