e41ebd1fab0f928581c1ea278dc09746ac57ff7e
Diana Lin <52944826+dianalin2@users.noreply.github.com>
2026-03-29 02:43:05 -0400

    fix thermal cutoff?

--- ESP-Projects/main/user_libs/src/tasks.c
+++ ESP-Projects/main/user_libs/src/tasks.c
@@ -49,8 +49,10 @@ void construction_robot_control_can_task()
         {
             motor_state = new_data;
         }
+        sendEn();
         directControl(motor_state);
-        vTaskDelay(pdMS_TO_TICKS(2));
+        ESP_ERROR_CHECK(twai_node_transmit_wait_all_done(g_node_hdl, TIMEOUT));
+        vTaskDelay(pdMS_TO_TICKS(5));
     }
 }
 
