5914157bde0f8fe462207384b2c9da58ff791845
jayradys <sokjayrady@gmail.com>
2026-03-30 21:43:42 -0400

    Revert "Changed UART and CAN config to work for S3 (not working)"
    This reverts commit f96dca920762169b05376ea810caa7174959acac.

--- ESP-Projects/main/user_libs/src/OneRobot.c
+++ ESP-Projects/main/user_libs/src/OneRobot.c
@@ -54,7 +54,6 @@ void canSetupTalons()
 void directControl(ControlPacket_OneRobot pkt)
 {
     int8_t leftSpeed = pkt.front_left_wheel;
-    sendEn();
     setTargetFX(&frontLeft, ((int8_t)(leftSpeed - 127)) * -1);
     setTargetFX(&backLeft, ((int8_t)(leftSpeed - 127)) * -1);
 
